788e3faff10124485df7e9e1a533b26de3c4ae22
Akhanrok <113940617+Akhanrok@users.noreply.github.com>
2023-05-31 09:34:51 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/Akhanrok/go_labs/database"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	// Initialize the database connection
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create a new instance of the router
+	router := mux.NewRouter()
+
+	// Serve static files from the "static" directory
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Start the HTTP server
+	log.Println("Server listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
