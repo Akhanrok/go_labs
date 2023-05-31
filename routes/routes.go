d63e2998ac9c04f02d80eb9e0d385557b2dea67b
Akhanrok <113940617+Akhanrok@users.noreply.github.com>
2023-05-31 09:05:27 +0300

    Fix routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,34 +1,31 @@
 package routes
 
 import (
-	"github.com/Akhanrok/go_labs/database"
-	"github.com/Akhanrok/go_labs/handlers"
+	"net/http"
 	"github.com/gorilla/mux"
+
+	"github.com/Akhanrok/go_labs/handlers"
+	"github.com/Akhanrok/go_labs/database"
+	
 )
 
 func RegisterRoutes(db *database.Database) *mux.Router {
 	router := mux.NewRouter()
 
 	userHandler := handlers.NewUserHandler(db)
-	productHandler := handlers.NewProductHandler(db)
-	shoppingListHandler := handlers.NewShoppingListHandler(db)
+	listHandler := handlers.NewListHandler(db)
 
 	// User routes
-	router.HandleFunc("/registerUser", userHandler.RegisterUser).Methods("POST")
-	router.HandleFunc("/loginUser", userHandler.LoginUser).Methods("POST")
-
-	// Product routes
-	router.HandleFunc("/product", productHandler.CreateProduct).Methods("POST")
-	router.HandleFunc("/product/{productID}", productHandler.GetProduct).Methods("GET")
-	router.HandleFunc("/product/{productID}", productHandler.UpdateProduct).Methods("PUT")
-	router.HandleFunc("/product/{productID}", productHandler.DeleteProduct).Methods("DELETE")
+	http.HandleFunc("/", userHandler.IndexHandler)
+	http.HandleFunc("/login", userHandler.LoginHandler)
+	http.HandleFunc("/register", userHandler.RegisterHandler)
+	http.HandleFunc("/register-success", userHandler.RegisterSuccessHandler)
+	http.HandleFunc("/login-success", userHandler.LoginSuccessHandler)
 
 	// Shopping list routes
-	router.HandleFunc("/shopping-list", shoppingListHandler.CreateShoppingList).Methods("POST")
-	router.HandleFunc("/shopping-list/{listID}/product", shoppingListHandler.AddProductToList).Methods("POST")
-	router.HandleFunc("/shopping-list/{listID}/product/{productID}", shoppingListHandler.RemoveProductFromList).Methods("DELETE")
-	router.HandleFunc("/shopping-list/{listID}/sort/type", shoppingListHandler.SortShoppingListByType).Methods("GET")
-	router.HandleFunc("/shopping-lists/{listID}/sort-by-type", shoppingListHandler.SortShoppingListByType)
+	http.HandleFunc("/create-list", listHandler.CreateListHandler)
+	http.HandleFunc("/list-success", listHandler.ListSuccessHandler)
+	http.HandleFunc("/view-lists", listHandler.ViewListsHandler)
 
 	return router
 }
